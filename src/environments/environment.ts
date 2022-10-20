a2ae7129339e186d95519c675fd8b46aab013742
Vinicius Lanzoni <viniciuslanzoni@tokenlab.com.br>
2022-10-20 19:54:38 -0300

    ReadMe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  cognitoUserPoolId: '',
-  cognitoAppClientId: '',
+  cognitoUserPoolId: 'us-east-1_lCu87ppy3',
+  cognitoAppClientId: '3n66garr0e6s8hrhf653tljdo4',
 };
